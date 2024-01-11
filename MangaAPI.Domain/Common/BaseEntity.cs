49c8f1667142ec66f41359ee86cc7c23dff0d4ca
blaineconnor <112892044+blaineconnor@users.noreply.github.com>
2024-01-11 11:47:57 +0300

    Update BaseEntity.cs
    Bunları auditable da tutmak daha iyi.

--- MangaAPI.Domain/Common/BaseEntity.cs
+++ MangaAPI.Domain/Common/BaseEntity.cs
@@ -4,7 +4,7 @@ namespace MangaAPI.Domain.Common
     public class BaseEntity
     {
         public Guid Id { get; set; }
-        public DateTime CreatedAt { get; set; }
+        
         public bool IsDeleted { get; set; }
     }
 }
